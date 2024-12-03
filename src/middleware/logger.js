6c6418fd2c197e361910fc100ad66a8a497d920b
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-12-03 13:09:33 -0700

    Nomas tenia que cambiar la imagen a build

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -15,11 +15,29 @@ client.connect().then(() => {
 });
 
 module.exports = (req, res, next) => {
+    let originalJson = res.json;
+    let originalSend = res.send;
+
+    res.json = function (body) {
+        res.body = body; 
+        return originalJson.call(this, body);
+    };
+
+    res.send = function (body) {
+        try {
+            res.body = JSON.parse(body);
+        } catch (e) {
+            res.body = body;
+        }
+        return originalSend.call(this, body); 
+    };
+
     res.on('finish', async () => {
         if (!client.isOpen) {
             console.error('Redis client -->> No conectado.');
             return;
         }
+
         const key = `${req.method}:${Date.now()}:${req.originalUrl}`;
         const logEntry = JSON.stringify({
             time: new Date(),
@@ -31,7 +49,8 @@ module.exports = (req, res, next) => {
             },
             res: {
                 statusCode: res.statusCode,
-                statusMessage: res.statusMessage
+                statusMessage: res.statusMessage,
+                body: res.body 
             }
         });
         try {
